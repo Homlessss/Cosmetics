0be6d96b202fe0450f99c60a19a2b114214739b4
Homlessss <mkhai2308@gmail.com>
2019-11-12 00:02:26 +0700

    comment

--- cosmetics/src/index.js
+++ cosmetics/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Header from './header';
 
+ReactDOM.render(<Header />, document.getElementsByClassName('header-content'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
